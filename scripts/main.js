c37eae021f213d7bd44fdd69504d853489734de4
ChristopheTreb <40229677+ChrisTreb@users.noreply.github.com>
2022-05-05 21:52:43 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
+// Menu
 jQuery('.burger-button').click(function() {
     jQuery("#menu").slideToggle();
+    jQuery(this).toggleClass('open');
 });
-
+// Cards
 jQuery(document).ready(function() {
     jQuery('.card-content').click(function() {
         jQuery(this).children('.card-hidden-content').slideToggle();
